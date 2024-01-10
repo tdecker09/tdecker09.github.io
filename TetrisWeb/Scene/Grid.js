dc9db6fba10f73015be2bb41467a2451dc51c337
tdecker09 <151651986+tdecker09@users.noreply.github.com>
2024-01-10 09:31:39 -0500

    added to grid class
    used tutorial to write grid class:
    setup with canvas, removed constructor, instantiated keyPresss

--- TetrisWeb/Scene/Grid.js
+++ TetrisWeb/Scene/Grid.js
@@ -1,8 +1,12 @@
-let board;
+let board; // array for canvas
+let graphic; // ?
+let canvas;
 
-class Grid {
-    constructor() {
-        <!--create 10x20 array !-->
+//for keyboard presses
+document.addEventListener('keydown', keyPress);
+
+    function setup() {
+        //create 10x20 array, piece location
         board = [];
         let rows = 10;
         let columns = 20;
@@ -13,12 +17,39 @@ class Grid {
                 board[r][c] = c;
             }
         }
+
+        //make canvas
+        canvas = document.getElementById('my-canvas');
+        graphic = canvas.getContext('2d')
+        //canvas.width =
+        //canvas.height =
+
+
+
     }
 
-    static isFilled(x, y) {
-        <!-- return true if given grid spot is occupied !-->
+    function isFilled(x, y) {
+        //return true if given grid spot is occupied
         return board[x][y] === 1;
     }
+
+    function drawShape(shape) {
+        // graphically draws shape to canvas based on location on board
+        // this method is supposed to have shape object param from shapeGen class !-->
+
+// LEFT OFF HERE: GET SHAPE OBJECT FROM SHAPEGEN CLASS, ACCESS ITS ARRAY FOR SHAPE STRUCTURE
+
+        //go through array, add element to correct spot on game array
+
+        //draw square at x + y coord on canvas
+        //graphic.fillStyle = [color]
+        //graphic.fillRect(x, y, [int], [int])
+
+
+    }
+
+    function keyPress() {
+
 }
 
 
