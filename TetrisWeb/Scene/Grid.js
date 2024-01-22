925d4a8de731d7800afbc557cbedfd32ee5a2998
tdecker09 <151651986+tdecker09@users.noreply.github.com>
2024-01-22 09:49:52 -0500

    shapes are one dimensional arrays
    this is bad

--- TetrisWeb/Scene/Grid.js
+++ TetrisWeb/Scene/Grid.js
@@ -54,9 +54,12 @@ document.addEventListener('keydown', keyPress);
         //graphic.fillRect(x, y, [int], [int])
 
         //start building shape in middle top of board
-        var boardRow, shapeRow, col;
-    ctx.fillText(shapeObj.typeIndex.toString(), 20, 20);
-        for(col = 1; col < 3; col++) {
+        let boardRow, shapeRow, col;
+
+
+
+
+        for(col = 0; col < 3; col++) {
             for(shapeRow = 0; shapeRow < 3; shapeRow++) {
                 boardRow = shapeRow + 3;
 
@@ -64,13 +67,17 @@ document.addEventListener('keydown', keyPress);
 
                 board[boardRow][col] = curShape.shapeType[shapeRow][col]; //transfer shape array onto board array
 
-                //graphically draw board
-                // ctx.fillStyle = "blue";
                 //if statement -> if index is true put a square
 
-                var test = true;
-                if(board[shapeRow][col] === 1) {
+                ctx.fillStyle = "rgba(0,100,72,0.47)";
+                ctx.fillRect(50*boardRow, 50*col, 50, 50);
 
+                var test = true;
+                // if(board[shapeRow][col] === 1) {
+
+
+                    ctx.fillText(col, 300, 20);
+                    ctx.fillText(shapeObj.typeIndex.toString(), 20, 20);//test
 
 
                     ctx.fillStyle = "rgb(0 100 0)";
@@ -80,7 +87,14 @@ document.addEventListener('keydown', keyPress);
 
                     ctx.fillText(col, 50*boardRow + 20*col, 10);
 
-                }
+                    let c = 0;
+                    for(let r = 0; r < 9; r++) {
+                        if(r / 3 === 0) {
+                            c ++;
+                            ctx.fillText(String.format(curShape.shapeType[r]), r * 20, 300 + (c * 20))
+                        }
+                    }
+
 
             }
         }
@@ -96,12 +110,14 @@ function load() {
         y: 0,
 
         //randomly select a shape from SHAPES array
-        typeIndex: Math.round(Math.random() * 6),
+        // typeIndex: Math.round(Math.random() * 6),
+        typeIndex: 0,
 
-        shapeType : null,
+
+        shapeType: SHAPES[1],//array
     }
 
-    shapeObj.shapeType = SHAPES[shapeObj.typeIndex];
+
 
     setup();
     drawShape();
